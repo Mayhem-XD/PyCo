ea542ce45d566297147a31c22410dd2be3b55779
Mayhem-XD <leahblo1994@gmail.com>
2023-08-13 23:50:14 +0900

    이미지업로드_수정해야함

--- bp_user/user.py
+++ bp_user/user.py
@@ -2,33 +2,13 @@ from flask import Blueprint , request, session, render_template
 from flask import redirect, flash
 import db.user_service as us
 import json, hashlib, base64
+from werkzeug.utils import secure_filename
+import os
+import utils as ut
 user_bp = Blueprint('user_bp',__name__)
+upload_dir = "d:/Temp/"
 
-# @user_bp.route('/login', methods=['GET','POST'])
-# def login():
-#     menu = {'ho':0,'nb':0,'us':1,'cr':0,'sc':0,'py':0}
-#     if request.method == 'GET':
-#         return render_template('/prototype/user/login.html',menu=menu)
-#     else:
-#         uid = request.form['uid']
-#         pwd = request.form['pwd']
-#         with open('static/data/password.txt') as f:
-#             s = f.read()
-#         passwords = json.loads(s)
-#         try:
-#             db_pwd = passwords[uid]
-#             pwd_sha256 = hashlib.sha256(pwd.encode())
-#             hashed_pwd = base64.b64encode(pwd_sha256.digest()).decode('utf-8')
-#             if hashed_pwd == db_pwd:
-#                 flash('good')
-#                 session['uid'] = uid        # 세션값을 설정함으로써 사용자가 로그인하였음을 알게 해줌
-#                 return redirect('/')
-#             else:
-#                 flash('잘못된 pwd')
-#                 return redirect('/user/login')
-#         except:
-#             flash('잘못된 id')
-#             return redirect('/user/register')
+
 @user_bp.route('/login', methods=['GET','POST'])
 def login():
     menu = {'ho':0,'nb':0,'us':1,'cr':0,'sc':0,'py':0}
@@ -56,17 +36,44 @@ def logout():
     session.pop('uid',None)                 # 설정한 세션값을 삭제
     return redirect('/')
 
-@user_bp.route('/register')
+@user_bp.route('/register', methods=['GET','POST'])
 def register():
     menu = {'ho':0,'nb':0,'us':1,'cr':0,'sc':0,'py':0}
     if request.method == 'GET':
         return render_template('/prototype/user/register.html',menu=menu)
     else:
         profile = request.files['profile']
+        print("profile : ",profile)
         uid = request.form['uid']
         pwd = request.form['pwd']
         pwd2 = request.form['pwd2']
         uname = request.form['uname']
         email = request.form['email']
-        emailDomain = request.form['emailDomain']
         addr = request.form['addr']
+        # uid check
+        user = us.get_user(uid=uid)
+        if user != None:
+            flash('중복된 ID')
+            return redirect('/user/register')
+        # pwd check
+        if (pwd == pwd2) and (len(pwd)>1):
+            pwd_sha256 = hashlib.sha256(pwd.encode())
+            hashed_pwd = base64.b64encode(pwd_sha256.digest()).decode('utf-8')
+
+            # if profile and profile.content_type.startswith('image/'):
+            #     filename = secure_filename(profile.filename)
+            #     profile_path = os.path.join(upload_dir, 'profile', filename)
+            #     profile.save(profile_path)
+            #     ut.center_image(profile).save(profile_path, format='png')
+            profile = None
+            params = (uid,hashed_pwd,uname,email,profile,addr)
+            print(params)
+
+            us.register_user(params=params)
+            flash('회원가입 완료')
+            return redirect('/user/login')
+        else:
+            flash('잘못된 password')
+            return redirect('/user/register')
+
+
