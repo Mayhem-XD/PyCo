cdcf7ca9aa6c16a428730289b82aab40c2626e38
Mayhem-XD <leahblo1994@gmail.com>
2023-08-13 21:39:14 +0900

    get_list

--- db/user_dao.py
+++ db/user_dao.py
@@ -25,3 +25,14 @@ def count_users():
     cur.close()
     conn.close()
     return row
+
+def get_user_list(page):
+    offset = (page - 1) * 10
+    conn = pool.get_connection()
+    cur = conn.cursor()
+    sql = "select * from users where isDeleted=0 order by regDate desc, uid limit 10 offset %s"
+    cur.execute(sql,(offset,))
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
