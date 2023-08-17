440e932b0b1c0eb15d2cc1bd882f5f7273a21b24
Mayhem-XD <leahblo1994@gmail.com>
2023-08-17 18:18:54 +0900

    board_service

--- db/board_service.py
+++ db/board_service.py
@@ -5,3 +5,14 @@ with open('./mysql.json') as f:
     config_str = f.read()
 config = json.loads(config_str)
 pool = pooling.MySQLConnectionPool(pool_name="mypool", pool_size=3, **config)
+
+def get_board(bid):
+    conn = pool.get_connection()
+    cur = conn.cursor()
+    sql = "SELECT b.bid, b.uid, b.title, b.content, b.modTime, b.viewCount, b.replyCount, b.files, u.uname \
+        FROM board AS b JOIN users AS u ON b.uid=u.uid WHERE b.bid=%s"
+    cur.execute(sql,(bid,))
+    row = cur.fetchone()
+    cur.close()
+    conn.close()
+    return row
