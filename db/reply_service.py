a18e82553fd891a344d104c8e384f225348415d8
Mayhem-XD <leahblo1994@gmail.com>
2023-08-18 19:11:06 +0900

    rDao

--- db/reply_service.py
+++ db/reply_service.py
@@ -5,3 +5,14 @@ with open('./mysql.json') as f:
     config_str = f.read()
 config = json.loads(config_str)
 pool = pooling.MySQLConnectionPool(pool_name="mypool", pool_size=3, **config)
+
+def get_reply_list(bid):
+    conn = pool.get_connection()
+    cur = conn.cursor()
+    sql = "SELECT r.rid, r.`comment`, r.regTime, r.isMine, r.uid, r.bid, u.uname \
+        FROM reply AS r JOIN users AS u ON r.uid=u.uid WHERE bid=%s"
+    cur.execute(sql,(bid,))
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
