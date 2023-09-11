3d0ebba774d918ea79e3e58bcb9c0fb088a3cbd6
Mayhem-XD <leahblo1994@gmail.com>
2023-09-11 21:52:38 +0900

    추가될_부분

--- bp_python/python_func.py
+++ bp_python/python_func.py
@@ -5,6 +5,7 @@ import utils as ut
 user_bp_p = Blueprint('user_bp_p',__name__)
 app = Flask(__name__)
 
+# 삭제하거나 대체될 예정
 @user_bp_p.route('/hotplace',methods=['GET','POST'])
 def hotplace():
     menu = {'ho':0,'nb':0,'us':0,'cr':0,'sc':0,'py':1}
@@ -17,4 +18,17 @@ def hotplace():
         places = [place1, place2, place3]
         ut.hot_places(places, app)
         return None
+
+# park_status
+@user_bp_p.route('/park_status',methods=['GET','POST'])
+def park_status():
+    menu = {'ho':0,'nb':0,'us':0,'cr':0,'sc':0,'py':1}
+    if request.method == 'GET':
+        return render_template('', menu=menu,weather=get_weather(app),quote=session['quote'],addr=session['addr'])
+    else:
+        pass
+        # ajax 예정
+
+
+
 
