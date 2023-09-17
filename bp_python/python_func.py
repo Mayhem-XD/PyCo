beb6e3a02f81d0a6e16ef70d2462167150ab3e73
Mayhem-XD <leahblo1994@gmail.com>
2023-09-17 22:04:44 +0900

    수정예정

--- bp_python/python_func.py
+++ bp_python/python_func.py
@@ -7,15 +7,18 @@ user_bp_p = Blueprint('user_bp_p',__name__)
 app = Flask(__name__)
 
 # 삭제하거나 대체될 예정
-@user_bp_p.route('/hotplace',methods=['GET','POST'])
+# 단순 검색
+@user_bp_p.route('/search',methods=['GET','POST'])
 def hotplace():
     menu = {'ho':0,'nb':0,'us':0,'cr':0,'sc':0,'py':1}
     if request.method == 'GET':
         return render_template('prototype/python/hotplace.html',menu=menu,weather=get_weather(app),quote=session['quote'],addr=session['addr'])
     else:
         place1 = request.form['place1']
+        # 수정할 부분
         place2 = request.form['place2']
         place3 = request.form['place3']
+        ########
         places = [place1, place2, place3]
         ut.hot_places(places, app)
         return None
