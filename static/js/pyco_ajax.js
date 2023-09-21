a3c3db89189dc6cfc351c0db8d55fd9a2bc8518a
Mayhem-XD <leahblo1994@gmail.com>
2023-09-21 23:27:40 +0900

    ajax

---
+++ static/js/pyco_ajax.js
@@ -0,0 +1,20 @@
+function send_val() {
+    let timep = $('#timep').val();
+    let stn = $('#input_stn').val()
+    $.ajax({
+        type: 'POST',
+        url: '/' ,      // 주소 추가해야함
+        data: { timep: timep, stn: stn},
+        success: function (result) {
+            $('#init_img').css('display', 'none');
+            $('#show_img').css('display', 'block');
+            let dn = result.dn;
+            let up = result.up;
+            $('#i1').attr('src', '/static/img/stage_'+dn+'.png');
+            $('#i2').attr('src', '/static/img/stage_'+up+'.png');
+
+        }
+
+    });
+}
+
