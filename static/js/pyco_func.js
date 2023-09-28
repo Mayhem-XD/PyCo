dc9d05bd80d68a1d242b4de411056fceefde5e8a
Mayhem-XD <leahblo1994@gmail.com>
2023-09-28 21:40:45 +0900

    func

--- static/js/pyco_func.js
+++ static/js/pyco_func.js
@@ -13,3 +13,35 @@ function search() {
     location.href = "/board/list?p={{session['current_page']}}&f=" + field + '&q=' + query;
 }
 // ######################################################
+
+// ################################# python func ##########################################
+$(document).ready(function() {
+    $('#example').DataTable({
+        info: false,
+        searching: true,
+        paging: true,
+        ordering: false,
+        //order: [[ 0, "asc" ]],
+        columnDefs: [{
+            "targets": [ 0 ],
+            "visible": false,
+            "searchable": false
+        }],
+        language: {
+            thousands: ',',
+            search: '',
+            searchPlaceholder: "검색어",
+            paginate: {
+                first: '처음',
+                previous: '이전',
+                next: '다음',
+                last: '마지막',
+            }
+        },
+        oLanguage: {
+            sLengthMenu: "_MENU_",
+        },
+        "lengthMenu": [[10, -1], ["10개씩 보기", "모두 보기"]]
+    });
+} );
+// ############################################################################
