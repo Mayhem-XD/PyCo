e41b0f2eb4646091ca3256149aa3450716155003
Mayhem-XD <leahblo1994@gmail.com>
2023-10-07 22:08:05 +0900

    dao

---
+++ db/schedule_service.py
@@ -0,0 +1,15 @@
+import json, hashlib, base64
+from mysql.connector import pooling
+
+def get_title():
+    pass
+def get_place():
+    pass
+def get_start_time():
+    pass
+def get_end_time():
+    pass
+def get_is_important():
+    pass
+def get_memo():
+    pass
