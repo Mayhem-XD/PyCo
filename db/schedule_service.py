95c1f18d9155e4fc134f64ec2c7fa956cc71ee4f
Mayhem-XD <leahblo1994@gmail.com>
2023-10-08 21:48:52 +0900

    service

--- db/schedule_service.py
+++ db/schedule_service.py
@@ -1,8 +1,20 @@
 import json, hashlib, base64
 from mysql.connector import pooling
+with open('./mysql.json') as f:
+    config_str = f.read()
+config = json.loads(config_str)
+pool = pooling.MySQLConnectionPool(pool_name="mypool", pool_size=3, **config)
 
-def get_title():
-    pass
+def get_schedule(sid):
+    conn = pool.get_connection()
+    cur = conn.cursor()
+    sql = "select * from schedule where sid = %s"
+    cur.execute(sql,(sid,))
+    row = cur.fetchone()
+    conn.commit()
+    cur.close()
+    conn.close()
+    return row
 def get_place():
     pass
 def get_start_time():
@@ -12,4 +24,6 @@ def get_end_time():
 def get_is_important():
     pass
 def get_memo():
+    pass
+def insert(params):
     pass
