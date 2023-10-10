40fe93682541b2f84a5f6cf418582a4573832206
Mayhem-XD <leahblo1994@gmail.com>
2023-10-10 20:29:20 +0900

    bug_fix

--- bp_schedule/schedule.py
+++ bp_schedule/schedule.py
@@ -25,18 +25,9 @@ def test():
 
 @user_bp_schedule.route('/detail/<sid>', methods=['GET'])
 def detail(sid):
-    schedule = sched.get_schedule(sid)
+    schedule_list = sched.get_schedule(sid)
     schedule_list = [dict(zip(['sid', 'title', 'place', 'startTime', 'endTime', 'isImportant', 'memo'], sch)) for sch in schedule_list]
-
-    # jSched = {
-    #     "sid": sid,
-    #     "title": schedule['title'],
-    #     "place": schedule['place'],
-    #     "startTime": schedule['startTime'],
-    #     "endTime": schedule['endTime'],
-    #     "isImportant": schedule['isImportant'],
-    #     "memo": schedule['memo']
-    # }
+    
     return jsonify(schedule_list)
 
 @user_bp_schedule.route('/delete/<sid>', methods=['GET'])
