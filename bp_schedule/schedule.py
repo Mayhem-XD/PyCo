2637820827909c5326785e57fc0522393cc777d7
Mayhem-XD <leahblo1994@gmail.com>
2023-10-12 19:26:11 +0900

    수정

--- bp_schedule/schedule.py
+++ bp_schedule/schedule.py
@@ -36,7 +36,7 @@ def delete(sid):
 
 @user_bp_schedule.route('/delete_confirm/<sid>', methods=['GET'])
 def delete_confirm(sid):
-    # ut.delete(sid)
+    sched.delete(sid)
     return redirect(url_for('user_bp_schedule.list'))
 
 @user_bp_schedule.route('/insert', methods=['POST'])
