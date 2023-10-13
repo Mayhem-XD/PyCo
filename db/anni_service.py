8f94cec6aca79cf33d3366c36f4483e67c1f95a8
Mayhem-XD <leahblo1994@gmail.com>
2023-10-13 18:45:44 +0900

    get_list

--- db/anni_service.py
+++ db/anni_service.py
@@ -5,8 +5,15 @@ with open('./mysql.json') as f:
 config = json.loads(config_str)
 pool = pooling.MySQLConnectionPool(pool_name="mypool", pool_size=3, **config)
 
-def get_anniv_list():
-    pass
+def get_anniv_list(params):
+    conn = pool.get_connection()
+    cur = conn.cursor()
+    sql = "SELECT * FROM anniversary WHERE adate >= %s and adate <= %s order by adate"
+    cur.execute(sql,params)
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
 
-def insert():
+def insert(params):
     pass
