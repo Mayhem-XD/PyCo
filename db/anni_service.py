218b9190db3b56df8629b1a360e3035e8319070e
Mayhem-XD <leahblo1994@gmail.com>
2023-10-13 18:43:10 +0900

    anniv

---
+++ db/anni_service.py
@@ -0,0 +1,12 @@
+import json, hashlib, base64
+from mysql.connector import pooling
+with open('./mysql.json') as f:
+    config_str = f.read()
+config = json.loads(config_str)
+pool = pooling.MySQLConnectionPool(pool_name="mypool", pool_size=3, **config)
+
+def get_anniv_list():
+    pass
+
+def insert():
+    pass
